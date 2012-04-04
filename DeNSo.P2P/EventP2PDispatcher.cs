e04ebe440360fc1dc1511c1653aa6e8f21278833
Paolo Possanzini <ppossanzini@gmail.com>
2012-04-05 01:13:20 +0200

    first version of P2P event dispatcher

---
+++ DeNSo.P2P/EventP2PDispatcher.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DeNSo.P2P.Services;
+using DeNSo.P2P.Interfaces;
+
+namespace DeNSo.P2P
+{
+  public static class EventP2PDispatcher
+  {
+    private static EventP2PServices _node = null;
+    private static IEventP2PServiceChannel _channel = null;
+
+    public static void EnableP2PEventMesh()
+    {
+      if (_channel != null && _channel.State == System.ServiceModel.CommunicationState.Opened) return;
+      _channel = Mesh.OpenPeerChannel<IEventP2PServices, IEventP2PServiceChannel>(_node = new EventP2PServices());
+    }
+
+    private static void RegisterP2PDispatcherInEventStore()
+    {
+      DeNSo.Core.EventHandlerManager.RegisterGlobalEventHandler(
+        (store, doc) =>
+        {
+          _channel.NewEventInTheMesh(new Messages.EventMessage()
+          {
+            NodeIdentity = DeNSo.Core.Configuration.NodeIdentity,
+            Database = store.DataBaseName,
+            Command = doc.Command,
+            CommandSN = doc.CommandSN
+          });
+        });
+    }
+
+    public static void StopP2PEventMesh()
+    {
+      if (_channel != null)
+      {
+        _channel.Close();
+      }
+    }
+  }
+}
---
+++ DeNSo.P2P/Mesh.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using DeNSo.P2P.Filters;
+
+namespace DeNSo.P2P
+{
+  internal static class Mesh
+  {
+    private static Uri GetURI()
+    {
+      return new Uri(DeNSo.Core.Configuration.PeerService);
+    }
+
+    public static TChannel OpenPeerChannel<TService, TChannel>(TService sourceObject) where TChannel : IClientChannel
+    {
+      InstanceContext sourceContext = new InstanceContext(sourceObject);
+      var portnumber = DeNSo.Core.Configuration.PeerNetworkPort;
+      NetPeerTcpBinding binding = new NetPeerTcpBinding()
+      {
+        Port = portnumber,
+        Name = GetURI().OriginalString + "@" + portnumber, 
+      };
+
+      binding.Security.Mode = SecurityMode.None;
+
+      EndpointAddress address = new EndpointAddress(GetURI().OriginalString);
+      DuplexChannelFactory<TChannel> sourceFactory = new DuplexChannelFactory<TChannel>(sourceContext, binding, address);
+      sourceFactory.Credentials.Peer.MeshPassword = DeNSo.Core.Configuration.PeerNetworkPassword;
+
+      TChannel sourceProxy = (TChannel)sourceFactory.CreateChannel();
+
+      MessagePropagationFilter remoteOnlyFilter = new MessagePropagationFilter();
+
+      PeerNode peerNode = ((IClientChannel)sourceProxy).GetProperty<PeerNode>();
+      peerNode.MessagePropagationFilter = remoteOnlyFilter;
+
+      sourceProxy.Open();
+      return sourceProxy;
+    }
+  }
+}
