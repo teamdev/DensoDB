7fa0e6e1f4e0e1432b531de6cedef386ec76bd4a
Paolo Possanzini <ppossanzini@gmail.com>
2012-04-19 15:22:02 +0200

    Implementing REST service

---
+++ DeNSo.Meta/Json/Bson2JsonExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeNSo.Meta.Json
+{
+  public static class Bson2JsonExtensions
+  {
+    public static string ToJson(this BSon.BSonDoc doc)
+    {
+      var sb = new StringBuilder();
+      if (doc != null)
+      {
+        sb.Append("{");
+        foreach (var propertyname in doc.Properties)
+        {
+          var property = doc[propertyname];
+          sb.AppendFormat("{0}:\"{1}\" ", propertyname,
+            property is BSon.BSonDoc ? ((BSon.BSonDoc)property).ToJson() : property);
+        }
+        sb.Append("}");
+      }
+      return sb.ToString(); 
+    }
+  }
+}
