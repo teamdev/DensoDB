547fcca70709d831e64e9c8ef53156124835ccf7
Paolo Possanzini <ppossanzini@gmail.com>
2012-04-27 11:30:49 +0200

    P2p Node Service

---
+++ DeNSo.P2P/Services/NodeService.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceModel;
+using DeNSo.P2P.Interfaces;
+using DeNSo.P2P.Messages;
+
+namespace DeNSo.P2P.Services
+{
+  [ServiceBehavior(InstanceContextMode = InstanceContextMode.Single)]
+  public class NodeService : INodeServices
+  {
+    #region private fields
+
+    private static Dictionary<Guid, NodeInfo> _nodes = new Dictionary<Guid, NodeInfo>();
+    private static Dictionary<Guid, INodeServices> _channels = new Dictionary<Guid, INodeServices>();
+    
+    #endregion
+
+    #region public properties
+
+    public static Dictionary<Guid, INodeServices> NodeChannels { get { return _channels; } }
+    public static Dictionary<Guid, NodeInfo> Nodes { get { return _nodes; } }
+
+    #endregion
+
+    protected INodeServices CallbackChannel
+    {
+      get
+      {
+        return OperationContext.Current.GetCallbackChannel<INodeServices>();
+      }
+    }
+
+    public void RequestNodeInfo()
+    {
+      CallbackChannel.NodeInfoResponse(NodeInfo.Current);
+    }
+    
+    public void NodeInfoResponse(NodeInfo node)
+    {
+      if (!Nodes.ContainsKey(node.Identity))
+      {
+        Nodes.Add(node.Identity, node);
+        NodeChannels.Add(node.Identity, CallbackChannel);
+      }
+      else
+      {
+        Nodes[node.Identity] = node;
+        NodeChannels[node.Identity] = CallbackChannel;
+      }
+    }
+  }
+}
