0a0e8912851f35a4af9749d314b2c3ead29ee990
Paolo Possanzini <ppossanzini@gmail.com>
2012-06-04 21:37:46 +0200

    Missing Consts class

---
+++ DeNSo.Meta/Consts.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeNSo.Meta
+{
+  public class Consts
+  {
+    public const string DensoIDKeyName = "@id";
+    public const string DensoTSKeyName = "_ts";
+  }
+}
