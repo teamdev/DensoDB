697d43f97919f51e25a5d83ac51670b31a871941
Paolo Possanzini <ppossanzini@gmail.com>
2012-06-09 15:15:04 +0200

    Refactoring of fixed command definition

---
+++ DeNSo.Meta/CommandKeyword.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DeNSo
+{
+  public class CommandKeyword
+  {
+    public const string Collection = "_collection";
+    public const string Action = "_action";
+    public const string Value = "_value";
+    public const string Id = "_id"; 
+    public const string Filter = "_filter";
+    public const string Type = "_type";
+  }
+}
